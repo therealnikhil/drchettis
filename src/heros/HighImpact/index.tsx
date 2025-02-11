467d9ced46c0deec01b393642dcc2faa3e35e559
Nikhil Kumar <nikhilfaips@gmail.com>
2025-02-11 09:32:06 -0600

    initial prototype

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -20,11 +20,11 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       className="relative -mt-[10.4rem] flex items-center justify-center text-white"
       data-theme="dark"
     >
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
+      <div className="container mb-8 z-10 relative flex items-center justify-start">
+        <div className="max-w-[36.5rem]">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
+            <ul className="flex md:justify-start gap-4">
               {links.map(({ link }, i) => {
                 return (
                   <li key={i}>
